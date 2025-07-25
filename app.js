3396b1da05be69ec25a0bb16d1953233713a1d31
Subudhi Roshni <subudhiroshni03@example.com>
2025-07-25 12:52:25 +0530

    The page has been made responsive.

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const sessionOptions={
     resave:false,
     saveUninitialized:true,
     cookie:{
-        expires:Date.now()+7*24*60*60*1000,
+        expires:new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         maxAge:7*24*60*60*1000,
         httpOnly:true,
     }
--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,4 @@
-// (() => {
-//   'use strict'
 
-//   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//   const forms = document.querySelectorAll('.needs-validation')
-
-//   // Loop over them and prevent submission
-//   Array.from(forms).forEach(form => {
-//     form.addEventListener('submit', event => {
-//       if (!form.checkValidity()) {
-//         event.preventDefault()
-//         event.stopPropagation()
-//       }
-
-//       form.classList.add('was-validated')
-//     }, false)
-//   })
-// })()
 
 document.addEventListener("DOMContentLoaded", () => {
   'use strict';
@@ -32,3 +15,18 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   });
 });
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  const tooltipList = [...tooltipTriggerList].map(el => new bootstrap.Tooltip(el));
+
+ const collapseEl = document.getElementById('navbarNavAltMarkup');
+
+  // When collapse is fully shown
+  collapseEl.addEventListener('show.bs.collapse', () => {
+    collapseEl.classList.add('navbar-blur');
+  });
+
+  // When collapse is fully hidden
+  collapseEl.addEventListener('hidden.bs.collapse', () => {
+    collapseEl.classList.remove('navbar-blur');
+  });
